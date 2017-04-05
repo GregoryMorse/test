a9dd9d41d63e377aeeb6a468856b2589fe9b5515
Gregory Morse <gregory.morse@live.com>
2017-04-05 15:52:03 +0200

    Update Main.java

--- selenium4/src/Main.java
+++ selenium4/src/Main.java
@@ -20,16 +20,17 @@ public class Main {
 
     @Test
     public void mainTest() {
-        System.setProperty("webdriver.chrome.driver", "c:/Users/hupqaai/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "c:/Users/morse/Downloads/chromedriver.exe");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 30);
 
 
         boolean result;
         try {
-            googleCalculatorTest("3 + 2","5");
-            googleCalculatorTest("3 * 2","6");
-            googleCalculatorTest("12 + 12","24");
+            //googleCalculatorTest("3 + 2","5");
+            //googleCalculatorTest("3 * 2","6");
+            //googleCalculatorTest("12 + 12","24");
+            firstTest();
         } catch(Exception e) {
             fail(e.getMessage());
         } finally {
@@ -47,4 +48,24 @@ public class Main {
 
         assertEquals(element.getText(),expected);
     }
+
+    //Run->Edit Configurations->Add selenium1-4 with respective .gradle project files, and build command
+    //in C:\Users\<userprofile>\.gradle\gradle.properties -> org.gradle.java.home=C:/Program Files (x86)/Java/jdk1.8.0_121
+    private static void firstTest()
+    {
+        driver.get("http://selenium.thinkcode.se/helloWorld");
+        WebElement el = driver.findElement(By.id("headline"));
+        assertEquals(el.getText(), "Hello, world!");
+
+        driver.get("http://selenium.thinkcode.se/requestPassword");
+        el = driver.findElement(By.id("account"));
+        el.sendKeys("test");
+        //el.sendKeys(Keys.RETURN);
+        driver.findElement(By.name("submit")).click();
+
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("confirmation")));
+
+        assertEquals(element.getText(), "A new password has been sent to test");
+    }
+
 }
